876aaaae205f659a827d7fd10e2c675ba1bef812
princetoad <princetoad@gmail.com>
2018-03-08 16:29:10 +0800

    import postcss-less; test case for variable convertion added

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-var postcss = require('postcss');
+const postcss = require('postcss');
+
+/**
+ * variables convertion
+ * @param root
+ */
+const handleVariables = (root) => {
+    // variables declaration
+    root.walkRules(rule => {
+        console.log('rule = ', rule);
+        console.log('rule.empty = ', rule.empty);
+    });
+    // variables reference
+};
 
 module.exports = postcss.plugin('postcss-less2scss', function (opts) {
     opts = opts || {};
 
     // Work with options here
 
-    return function (root, result) {
-
+    return function (root) {
         // Transform CSS AST here
-
+        handleVariables(root);
     };
 });
--- index.test.js
+++ index.test.js
@@ -1,19 +1,55 @@
 var postcss = require('postcss');
+var less = require('postcss-less');
 
 var plugin = require('./');
 
 function run(input, output, opts) {
-    return postcss([ plugin(opts) ]).process(input)
+    return postcss([plugin(opts)]).process(input, { syntax: less })
         .then(result => {
             expect(result.css).toEqual(output);
             expect(result.warnings().length).toBe(0);
         });
 }
 
-/* Write tests here
+/**
+ * variables
+ */
+it('variables - declaration & reference', () => {
+    var input = `
+// Variables
+@link-color:        #428bca; // sea blue
+@link-color-hover:  darken(@link-color, 10%);
 
-it('does something', () => {
-    return run('a{ }', 'a{ }', { });
+// Usage
+a,
+.link {
+  color: @link-color;
+}
+a:hover {
+  color: @link-color-hover;
+}
+.widget {
+  color: #fff;
+  background: @link-color;
+}
+`;
+    var output = `
+// Variables
+$link-color:        #428bca; // sea blue
+$link-color-hover:  darken($link-color, 10%);
+
+// Usage
+a,
+.link {
+  color: $link-color;
+}
+a:hover {
+  color: $link-color-hover;
+}
+.widget {
+  color: #fff;
+  background: $link-color;
+}
+`;
+    return run(input, output, {});
 });
-
-*/
