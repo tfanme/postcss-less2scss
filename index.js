60a6ac1e3643cce0027e97cfe2da5e65e5ad9252
princetoad <princetoad@gmail.com>
2018-03-10 10:21:47 +0800

    testcase of variables for at-rule added

--- index.js
+++ index.js
@@ -6,16 +6,29 @@ const postcss = require('postcss');
  */
 const handleVariables = (root) => {
     // variables declaration
+    root.walkAtRules(atRule => {
+        console.log('atRule = ', atRule);
+    });
     root.walkRules(rule => {
         console.log('rule = ', rule);
         console.log('rule.empty = ', rule.empty);
+    });
+    root.walkDecls(decl => {
+        console.log('decl.prop = ', decl.prop, ', decl.value = ', decl.value);
+        const variableRegex = /@/;
+        if (decl.prop.match(variableRegex)) {
+            decl.prop = decl.prop.replace(variableRegex, '$');
+        }
+        if (decl.value.match(variableRegex)) {
+            decl.value = decl.value.replace(variableRegex, '$');
+        }
     });
     // variables reference
 };
 
 module.exports = postcss.plugin('postcss-less2scss', function (opts) {
     opts = opts || {};
-
+    console.log('opts = ', opts);
     // Work with options here
 
     return function (root) {
